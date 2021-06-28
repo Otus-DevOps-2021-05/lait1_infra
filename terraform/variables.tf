dafb36830e6654e208b82eaa59deeb45f692b0ad
aeremeev <aeremeev@ourgapps.com>
2021-06-28 18:55:19 +0300

    Formating

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,32 +1,25 @@
 variable cloud_id {
   description = "Cloud"
-//  default = "b1g8e8hfocufl49sa83c"
 }
 variable folder_id {
   description = "Folder"
-//  default = "b1gg7tvrrsu1sct06vh4"
 }
 variable zone {
   description = "Zone"
-  default = "ru-central1-a"
+  default     = "ru-central1-a"
 }
 variable public_key_path {
   description = "Path to the public key used for ssh access"
-//  default = "~/.ssh/appuser.pub"
 }
 variable private_key_path {
   description = "Path to the public key used for ssh access"
-//  default = "~/.ssh/appuser"
 }
 variable image_id {
   description = "Disk image"
-//  default = "fd8mnqkfb6pqo93qtg3u"
 }
 variable subnet_id {
   description = "Subnet"
-//  default = 'e9bgjuqpjf3sdtdbf02r'
 }
 variable token {
   description = "Token"
-//  default = "AQAAAAADujTKAATuwT4xhASiSEVivf9CLQ5oZv0"
 }
