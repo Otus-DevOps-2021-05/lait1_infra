f27b9b140b5bd356c94b1ef48e2f33369e67cf02
aeremeev <aeremeev@ourgapps.com>
2021-07-01 14:59:13 +0300

    division into modules

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -30,5 +30,13 @@ variable token {
 }
 variable instance_count {
   description = "count instances"
-  default     = 2
+  default     = 1
+}
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default = "reddit-app-base"
+}
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default = "reddit-db-base"
 }
